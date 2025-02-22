a9d7a4aa1fdda6f886c1f9dc4d236edd1c9ee23b
Divij Ningu <divijningu@gmail.com>
2025-02-23 01:33:53 +0530

    added page level scripts section in the layouts

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -322,3 +322,12 @@
         </div>
     </div>
 @endsection
+
+@section('page-level-scripts')
+    <!-- Page level plugins -->
+    <script src="{{ asset('admin/vendor/chart.js/Chart.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ asset('admin/js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('admin/js/demo/chart-pie-demo.js') }}"></script>
+@endsection
