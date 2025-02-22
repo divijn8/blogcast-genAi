ae3375382cb240610f178554ed3848edc75afcc9
Divij Ningu <divijningu@gmail.com>
2025-02-23 01:29:39 +0530

    Published vendor files and link Bootstrap 5 paginator

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -29,7 +29,7 @@
     <div class="card-footer">
         <div class="row">
             <div class="col-md-12">
-                {{$categories->links()}}
+                {{$categories->links('vendor.pagination.bootstrap-5')}}
             </div>
         </div>
     </div>
