220b52fd00dc3152912f244054f7b89cc1829443
Divij Ningu <divijningu@gmail.com>
2025-02-24 22:45:49 +0530

    chore: - Added relationship to category and tags in Post Model. - Added relationship to posts in Category and Tag Model.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable=['title','excerpt','body','thumbnail','category_id'];
+
+    public function tags() {
+        return $this-> belongsToMany(Tag::class);
+    }
+
+    public function category() {
+        return $this-> belongsTo(Category::class);
+    }
 }
