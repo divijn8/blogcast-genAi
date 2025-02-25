48cad70f1cb781ea70da0489cc1e61d9b63165df
Divij Ningu <divijningu@gmail.com>
2025-02-26 01:15:47 +0530

    fix: - added mutator which adds slug automatically on creation and editing of post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,17 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable=['title','excerpt','body','thumbnail','category_id'];
+    protected $fillable=['title','slug','excerpt','body','thumbnail','category_id'];
 
      // Accessors:for consistent and customized data presentation.
      public function getThumbnailPathAttribute() {
         return 'storage/'.$this->thumbnail;
     }
+
+    // Mutators: to modify or set an attribute's value before it is saved to the database.
+    public function setTitleAttribute(string $title) {
+        $this->attributes['title'] = $title;
+        $this->attributes['slug'] = Str::slug($title);
+    }
+
 
     public function tags() {
         return $this-> belongsToMany(Tag::class);
