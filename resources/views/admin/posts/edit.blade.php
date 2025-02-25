b172430e383a448d6552435b4b63091f173e14b1
Divij Ningu <divijningu@gmail.com>
2025-02-26 00:10:41 +0530

    CRUD Operations for post

---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,120 @@
+@extends('admin.layouts.app')
+
+@section('page-level-styles')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
+
+    <style>
+        .is-invalid+.select2 > .selection > .select2-selection.select2-selection--multiple {
+            border: solid 1px red;
+        }
+    </style>
+@endsection
+
+
+@section('main-content')
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Edit Post</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{ route('admin.posts.update', $post) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Title</label>
+                            <input type="text"
+                                   class="form-control @error('title')is-invalid @enderror"
+                                   name="title"
+                                   value="{{old('title', $post->title)}}"
+                                   id="title"/>
+                            @error('title')
+                            <span class="text-danger text-sm">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="excerpt" class="form-label">Excerpt</label>
+                            <textarea class="form-control @error('excerpt')is-invalid @enderror"
+                                      name="excerpt"
+                                      id="excerpt"
+                            >{{old('excerpt', $post->excerpt)}}</textarea>
+                            @error('excerpt')
+                            <span class="text-danger text-sm">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="body" class="form-label">Body</label>
+                            <trix-editor input="body"></trix-editor>
+                            <input type="hidden"
+                                   class="form-control @error('body')is-invalid @enderror"
+                                   name="body"
+                                   id="body"
+                                   value="{{old('body', $post->body)}}"
+                            />
+                            @error('body')
+                            <span class="text-danger text-sm">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="thumbnail" class="form-label">Thumbnail</label>
+                            <input type="file"
+                                   accept="image/*"
+                                   class="form-control @error('thumbnail')is-invalid @enderror"
+                                   name="thumbnail"
+                                   value="{{old('thumbnail')}}"
+                                   id="thumbnail"/>
+                            @error('thumbnail')
+                                <span class="text-danger text-sm">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="category_id" class="form-label">Category</label>
+                            <select class="form-control select2 @error('category_id') is-invalid @enderror"
+                                    name="category_id"
+                                    id="category_id"
+                            >
+                                <option value="0" disabled selected>Select Category....</option>
+                                @foreach($categories as $category)
+                                    <option value="{{ $category->id }}" {{ $category->id == old('category_id', $post->category_id) ? 'selected' : '' }}>{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('category_id')
+                            <span class="text-danger text-sm">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="tag_id" class="form-label">Tags</label>
+                            <select class="form-control select2 @error('tags') is-invalid @enderror"
+                                    name="tags[]"
+                                    id="tag_id"
+                                    multiple
+                            >
+                                @foreach($tags as $tag)
+                                    <option value="{{ $tag->id }}" {{ in_array($tag->id, old('tags', $post->tags->pluck('id')->toArray())) ? 'selected' : '' }}>{{ $tag->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('tags')
+                            <span class="text-danger text-sm">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <input type="submit" class="btn btn-outline-primary" value="Update Post">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@section('page-level-scripts')
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
+    <script>
+        $('.select2').select2();
+    </script>
+@endsection
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,108 @@
+@extends('admin.layouts.app')
+
+@section('title',"Pen It - Posts")
+
+@section('main-content')
+    <!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Posts</h1>
+    <a href={{ route('admin.posts.create') }} class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            class="fas fa-cog fa-sm text-white-50"></i> Create Post</a>
+</div>
+@include('admin.layouts._alerts')
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <th>Id</th>
+                        <th>Thumbnail</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Tags</th>
+                        <th>Actions</th>
+                    </thead>
+                    <tbody>
+                        @foreach($posts as $post)
+                                <tr>
+                                    <td>{{ $post->id }}</td>
+                                    <td width="20%">
+                                        <img src="{{env('APP_URL')}}/storage/{{ $post->thumbnail }}" alt="{{ $post->title }}" class="img-fluid">
+                                    </td>
+                                    <td>{{ $post->title }}</td>
+                                    <td>{{ $post->category->name }}</td>
+                                    <td>{{ implode(', ', $post->tags->pluck('name')->toArray()) }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.posts.edit', $post) }}" class="btn btn-outline-warning">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        <button class="btn btn-outline-danger delete-post" data-toggle="modal" data-target="#deleteModal" data-post-id="{{ $post->id }}">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer">
+        <div class="row">
+            <div class="col-md-12">
+                {{$posts->links('vendor.pagination.bootstrap-5')}}
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Delete Modal</h1>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you wanna delete this post?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <form action="" method="POST" id="deleteForm">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete it!</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('page-level-scripts')
+    <script>
+        const deletePostButtons = document.querySelectorAll('.delete-post');
+        const deleteForm = document.querySelector("#deleteForm");
+        function loadDeleteModal(evt) {
+            let postId = null;
+            if(evt.target.classList.contains('delete-post')) {
+                postId = evt.target.dataset.postId;
+            } else if(evt.target.parentElement.classList.contains('delete-post')) {
+                postId = evt.target.parentElement.dataset.postId;
+            }
+            deleteForm.setAttribute('action', `/admin/posts/${postId}`);
+        }
+
+        deletePostButtons.forEach(function(deletePostButton) {
+            deletePostButton.addEventListener('click', loadDeleteModal);
+        });
+    </script>
+@endsection
+
+
