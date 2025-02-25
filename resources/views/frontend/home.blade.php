e683e1e241651bbff0528ffb4a27fa6ba7a88334
Divij Ningu <divijningu@gmail.com>
2025-02-26 01:04:52 +0530

    Made partials for frontend theme And added pagination for Frontend ui

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -14,22 +14,12 @@
                {{ $post->excerpt}}
             </p>
             <a href="#" class="button button-gray button-xs">Read More <i class="fa fa-long-arrow-right"></i></a>
-            
+
         </div>
         @endforeach
     </div>
 
     <!-- Blog Paging
     ===================================== -->
-    <div class="row mt25 animated" data-animation="fadeInUp" data-animation-delay="100">
-        <div class="col-md-6">
-            <a href="#" class="button button-sm button-pasific pull-left hover-skew-backward">
-                Old Entries
-            </a>
-        </div>
-        <div class="col-md-6">
-            <a href="#" class="button button-sm button-pasific pull-right hover-skew-forward">New Entries</a>
-        </div>
-    </div>
-
+    {{ $posts->links('frontend.partials._pagination') }}
 @endsection
