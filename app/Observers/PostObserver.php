dbac44db5a3367240db3c0a2db556e0ebe73c476
Divij Ningu <divijningu@gmail.com>
2025-03-02 03:47:14 +0530

    feat: - added a new column named posts_count to categories to avoid n+1 queries. - Now to update this column, we used observer pattern. In Laravel we can create observer using:   -- php artisan make:observer PostObserver --model=Post

---
+++ app/Observers/PostObserver.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Category;
+use App\Models\Post;
+
+class PostObserver
+{
+    /**
+     * Handle the Post "created" event.
+     */
+    public function created(Post $post): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Post "updated" event.
+     */
+    public function updated(Post $post): void
+    {
+        if($post->isDirty('category_id')) {
+            Category::where('id', $post->getOriginal('category_id'))->decrement('posts_count');
+            Category::where('id', $post->category_id)->increment('posts_count');
+        }
+    }
+
+
+    /**
+     * Handle the Post "deleted" event.
+     */
+    public function deleted(Post $post): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Post "restored" event.
+     */
+    public function restored(Post $post): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Post "force deleted" event.
+     */
+    public function forceDeleted(Post $post): void
+    {
+        //
+    }
+}
