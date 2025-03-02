cd86ce9895ae9e247355e9525609123994fee51b
Divij Ningu <divijningu@gmail.com>
2025-03-02 05:43:52 +0530

    chore: - Added csrf token in meta tag so it can be used in JS without being passed from php file

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -18,6 +18,8 @@
     <!-- Custom styles for this template-->
     <link href="{{ asset('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @yield('page-level-styles')
 </head>
 
