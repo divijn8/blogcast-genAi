6b8c0f31ea0a6b7e45bae64ce13433921bacaf79
Divij Ningu <divijningu@gmail.com>
2025-07-25 13:14:35 +0530

    chore:added route for comments

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,5 @@ Route::middleware(['auth'])->group((function() {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/comments', [App\Http\Controllers\CommentController::class, 'store'])->name('comments.store')->middleware('auth');
+
