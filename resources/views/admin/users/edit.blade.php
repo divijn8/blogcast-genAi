41437758b1034a83acd10f6f31f1907cfd25dc33
Ayush Dulani <onlyyforstudy@gmail.com>
2026-01-24 15:07:05 +0530

    fix: heading of the page

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,7 +5,7 @@
 @section('main-content')
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Update Tag</h1>
+        <h1 class="h3 mb-0 text-gray-800">Update User Details</h1>
     </div>
     <div class="row">
         <div class="col-md-12">
